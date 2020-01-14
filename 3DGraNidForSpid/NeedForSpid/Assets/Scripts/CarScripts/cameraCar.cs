f6d07a1c2dee1ced74200a7712dfb50898e4d156
Norbert <norbertskulski@wp.pl>
2020-01-15 00:02:09 +0100

    zmiana kamery pod c

---
+++ 3DGraNidForSpid/NeedForSpid/Assets/Scripts/CarScripts/cameraCar.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraCar : MonoBehaviour
+{
+    public Transform car;
+    public float distance;
+    public float height;
+    public float rotationDamping;
+    public float heightDamping;
+    public float zoomRatio;
+    public float defaultFOV;
+
+    public float rotation_v;
+
+
+    void FixedUpdate()
+    {
+        Vector3 local_velocity = car.InverseTransformDirection(car.GetComponent<Rigidbody>().velocity);
+        if (local_velocity.z < -0.5f)
+        {
+            rotation_v = car.eulerAngles.y + 100;
+        }
+        else
+        {
+            rotation_v = car.eulerAngles.y;
+        }
+
+        float acceleration = car.GetComponent<Rigidbody>().velocity.magnitude;
+
+        Camera.main.fieldOfView = defaultFOV + acceleration * zoomRatio * Time.deltaTime;
+
+    }
+
+    void LateUpdate()
+    {
+        float wantedAngle = rotation_v;
+
+        float wantedHeight = car.position.y + height;
+        
+        float myAngle = transform.eulerAngles.y;
+        float myHeigh = transform.position.y;
+
+        myAngle = Mathf.LerpAngle(myAngle, wantedAngle, rotationDamping + Time.deltaTime);
+        myHeigh = Mathf.LerpAngle(myHeigh, wantedHeight, heightDamping + Time.deltaTime);
+
+        Quaternion currentRotation = Quaternion.Euler(0, myAngle, 0);
+
+        transform.position = car.position;
+        transform.position -= currentRotation * Vector3.forward *distance;
+
+        Vector3 temp = transform.position;
+        temp.y = myHeigh;
+        temp.x = car.position.x + -5.0f;//temp.x + -3.0f;
+        transform.position = temp;
+
+        //transform.LookAt(car);
+
+    }
+}
---
+++ 3DGraNidForSpid/NeedForSpid/Assets/Scripts/CarScripts/switchCamera.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchCamera : MonoBehaviour
+{
+    public GameObject cameraOne;
+    public GameObject cameraTwo;
+    public GameObject cameraThree;
+
+    AudioListener cameraOneAudioLis;
+    AudioListener cameraTwoAudioLis;
+    AudioListener cameraThreeAudioLis;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //Get Camera Listeners
+        cameraOneAudioLis = cameraOne.GetComponent<AudioListener>();
+        cameraTwoAudioLis = cameraTwo.GetComponent<AudioListener>();
+        cameraThreeAudioLis = cameraThree.GetComponent<AudioListener>();
+
+        //Camera Position Set
+        cameraPositionChange(PlayerPrefs.GetInt("CameraPosition"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Change Camera Keyboard
+        _switchCamera();
+    }
+
+    //UI JoyStick Method
+    public void cameraPositonM()
+    {
+        cameraChangeCounter();
+    }
+
+    //Change Camera Keyboard
+    void _switchCamera()
+    {
+        if (Input.GetKeyDown(KeyCode.C) || Input.GetKeyDown(KeyCode.LeftAlt) || Input.GetKeyDown(KeyCode.RightAlt))
+        {
+            cameraChangeCounter();
+        }
+    }
+
+    //Camera Counter
+    void cameraChangeCounter()
+    {
+        int cameraPositionCounter = PlayerPrefs.GetInt("CameraPosition");
+        cameraPositionCounter++;
+        cameraPositionChange(cameraPositionCounter);
+    }
+
+    //Camera change Logic
+    void cameraPositionChange(int camPosition)
+    {
+        if (camPosition > 2)
+        {
+            camPosition = 0;
+        }
+
+        //Set camera position database
+        PlayerPrefs.SetInt("CameraPosition", camPosition);
+
+
+        switch (camPosition)
+        {
+            case 0:
+                cameraOne.SetActive(true);
+                cameraOneAudioLis.enabled = true;
+
+                cameraTwoAudioLis.enabled = false;
+                cameraTwo.SetActive(false);
+                cameraThreeAudioLis.enabled = false;
+                cameraThree.SetActive(false);
+                break;
+            case 1:
+                cameraTwo.SetActive(true);
+                cameraTwoAudioLis.enabled = true;
+
+                cameraOneAudioLis.enabled = false;
+                cameraOne.SetActive(false);
+                cameraThreeAudioLis.enabled = false;
+                cameraThree.SetActive(false);
+                break;
+            case 2:
+                cameraThree.SetActive(true);
+                cameraThreeAudioLis.enabled = true;
+
+                cameraOneAudioLis.enabled = false;
+                cameraOne.SetActive(false);
+                cameraTwoAudioLis.enabled = false;
+                cameraTwo.SetActive(false);
+                break;
+        }
+    }
+}
